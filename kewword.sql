ac6cee88356c70c16b37b12b656f77545a938ba1
Shalukumari122 <shalu.kumari.actowiz@gmail.com>
2024-10-02 18:25:57 +0530

    Add files via upload

---
+++ kewword.sql
@@ -0,0 +1,28 @@
+/*
+SQLyog Professional v13.1.1 (64 bit)
+MySQL - 8.0.33 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `keyword` (
+	`Keywords` text ,
+	`id` int (11)
+); 
+insert into `keyword` (`Keywords`, `id`) values('Cake','1');
+insert into `keyword` (`Keywords`, `id`) values('Cakes','2');
+insert into `keyword` (`Keywords`, `id`) values('Cookie','3');
+insert into `keyword` (`Keywords`, `id`) values('Cookies','4');
+insert into `keyword` (`Keywords`, `id`) values('Bread','5');
+insert into `keyword` (`Keywords`, `id`) values('Breads','6');
+insert into `keyword` (`Keywords`, `id`) values('Snack','7');
+insert into `keyword` (`Keywords`, `id`) values('Snacks','8');
+insert into `keyword` (`Keywords`, `id`) values('Rusk','9');
+insert into `keyword` (`Keywords`, `id`) values('Rusks','10');
+insert into `keyword` (`Keywords`, `id`) values('Toast','11');
+insert into `keyword` (`Keywords`, `id`) values('Cracker','12');
+insert into `keyword` (`Keywords`, `id`) values('Crackers','13');
+insert into `keyword` (`Keywords`, `id`) values('The Bakers Dozen','14');
+insert into `keyword` (`Keywords`, `id`) values('The Baker\'s Dozen','15');
+insert into `keyword` (`Keywords`, `id`) values('Bakers Dozen','16');
+insert into `keyword` (`Keywords`, `id`) values('Baker\'s Dozen','17');
---
+++ pincodes.sql
@@ -0,0 +1,31 @@
+/*
+SQLyog Professional v13.1.1 (64 bit)
+MySQL - 8.0.33 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `pincodes` (
+	`city` text ,
+	`pincode` bigint (20),
+	`storeid` varchar (75),
+	`address` text ,
+	`lat` double ,
+	`long` double 
+); 
+insert into `pincodes` (`city`, `pincode`, `storeid`, `address`, `lat`, `long`) values('Delhi','110017','1382291','New Delhi, Delhi 110017, India','28.5279118','77.2088986999999');
+insert into `pincodes` (`city`, `pincode`, `storeid`, `address`, `lat`, `long`) values('Delhi','110048',NULL,NULL,NULL,NULL);
+insert into `pincodes` (`city`, `pincode`, `storeid`, `address`, `lat`, `long`) values('Kolkata','700075',NULL,NULL,NULL,NULL);
+insert into `pincodes` (`city`, `pincode`, `storeid`, `address`, `lat`, `long`) values('Mumbai','400050','969383','Mumbai, Maharashtra 400050, India','19.0551695','72.8299518');
+insert into `pincodes` (`city`, `pincode`, `storeid`, `address`, `lat`, `long`) values('Mumbai','400053',NULL,NULL,NULL,NULL);
+insert into `pincodes` (`city`, `pincode`, `storeid`, `address`, `lat`, `long`) values('Mumbai','400057',NULL,NULL,NULL,NULL);
+insert into `pincodes` (`city`, `pincode`, `storeid`, `address`, `lat`, `long`) values('Pune','411014','1389691','Pune, Maharashtra 411014, India','18.5574028','73.9283004999999');
+insert into `pincodes` (`city`, `pincode`, `storeid`, `address`, `lat`, `long`) values('Pune','411057',NULL,NULL,NULL,NULL);
+insert into `pincodes` (`city`, `pincode`, `storeid`, `address`, `lat`, `long`) values('Pune','412202',NULL,NULL,NULL,NULL);
+insert into `pincodes` (`city`, `pincode`, `storeid`, `address`, `lat`, `long`) values('Hyderabad','500014','1388312','Nizamabad - Hyderabad Rd, Sai Nagar, Kompally, Hyderabad, Telangana, India','17.5487065','78.4930217');
+insert into `pincodes` (`city`, `pincode`, `storeid`, `address`, `lat`, `long`) values('Hyderabad','500084','1386094','Hyderabad, Telangana 500084, India','17.4707751','78.3587426');
+insert into `pincodes` (`city`, `pincode`, `storeid`, `address`, `lat`, `long`) values('Hyderabad','501501',NULL,NULL,NULL,NULL);
+insert into `pincodes` (`city`, `pincode`, `storeid`, `address`, `lat`, `long`) values('Bangalore','560066','762610','Bengaluru, Karnataka 560066, India','12.9698066','77.7499632');
+insert into `pincodes` (`city`, `pincode`, `storeid`, `address`, `lat`, `long`) values('Bangalore','560078','1237709','Bengaluru, Karnataka 560078, India','12.898773','77.5764094');
+insert into `pincodes` (`city`, `pincode`, `storeid`, `address`, `lat`, `long`) values('Bangalore','560102','1231052','4th Cross Rd, Sector 5, HSR Layout, Bengaluru, Karnataka 560102, India','12.911862','77.6445923');
+insert into `pincodes` (`city`, `pincode`, `storeid`, `address`, `lat`, `long`) values('Kolkata','700027',NULL,NULL,NULL,NULL);
